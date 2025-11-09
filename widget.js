389dba7b89975a18cd5aa7f91524f51efb676673
SaifSyed08 <saifsyed2999@gmail.com>
2025-11-09 15:56:28 -0600

    Add iframe for payment calculator widget

---
+++ widget.js
@@ -0,0 +1,11 @@
+(function() {
+  const iframe = document.createElement("iframe");
+  iframe.src = "https://yourusername.github.io/payment-calculator/";
+  iframe.style.border = "none";
+  iframe.style.width = "100%";
+  iframe.style.height = "600px";
+  iframe.style.borderRadius = "10px";
+  iframe.loading = "lazy";
+
+  document.currentScript.parentNode.insertBefore(iframe, document.currentScript);
+})();
